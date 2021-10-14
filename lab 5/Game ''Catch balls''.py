5e92d1a5837c77da76869f4a5a2e717afbac0d22
DmitrenkoSasha <dmitrenko.am@phystech.edu>
2021-10-15 00:47:58 +0300

    Изменения с урока 09.10

--- lab 5/Game ''Catch balls''.py
+++ lab 5/Game ''Catch balls''.py
@@ -4,7 +4,7 @@ from random import randint
 pygame.init()
 
 FPS = 2
-screen = pygame.display.set_mode((1200, 900))
+screen = pygame.display.set_mode((1200, 600))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -30,11 +30,26 @@ def new_ball():
 
 def click():
     """
-
     :return: Пишет "Click!", а на следующей строке координаты шарика
     """
     print('Click!')
     print(x, y, r)
+    print(type(event.pos))
+    print(event.pos[0])
+
+
+
+def check(x_click, y_click):
+    """
+    Проверяет, попал ли пользователь курсором в шарик
+    :param x_click: координата нажатия курсора по горизонтали
+    :param y_click: координата нажатия курсора по горизонтали
+    :return:
+    """
+    x_click = event.pos([0])  # Из кортежа с двумя элементами event.pos() вытаскиваем первый, соотв. горизонтали
+    y_click = event.pos([1])  # Из кортежа с двумя элементами event.pos() вытаскиваем второй, соотв. вертикали
+    if (x_click-x)**2 + (y_click-y)**2 < r**2:
+        k+=1
 
 
 pygame.display.update()
