03225174bbacd0d7fbefc4d80ab457d97961b45c
DmitrenkoSasha <dmitrenko.am@phystech.edu>
2021-10-15 17:01:47 +0300

    Создал 2 списка скоростей по х и y. Внёс изменения в функции move_x, move_y так, чтобы шарики отражались от стенок

--- lab 5/Game ''Catch balls''.py
+++ lab 5/Game ''Catch balls''.py
@@ -1,9 +1,10 @@
 import pygame
 from pygame.draw import *
 from random import randint, uniform
+
 pygame.init()
 
-FPS = 2
+FPS = 20
 screen = pygame.display.set_mode((800, 600))
 
 RED = (255, 0, 0)
@@ -15,13 +16,20 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-k=0 #  кол-во попаданий
-c=0 #   кол-во шариков в списке
+k = 0  # кол-во попаданий
+c = 0  # кол-во шариков в списке
+x = 0
+y = 0
+color = 0
+r = 0
 
 pool_x = []
 pool_y = []
 pool_r = []
 pool_color = []
+pool_vx = []
+pool_vy = []
+
 
 def new_ball():
     """
@@ -41,31 +49,37 @@ def check():
     :return: "Попал!" или "Мимо!"
     """
     global k
-    x_click = pygame.mouse.get_pos()[0]  #координата нажатия курсора по горизонтали
-    y_click = pygame.mouse.get_pos()[1]  #координата нажатия курсора по вертикали
-    if (x_click-x)**2 + (y_click-y)**2 < r**2:
-        k+=1
+    x_click = pygame.mouse.get_pos()[0]  # координата нажатия курсора по горизонтали
+    y_click = pygame.mouse.get_pos()[1]  # координата нажатия курсора по вертикали
+    if (x_click - x) ** 2 + (y_click - y) ** 2 < r ** 2:
+        k += 1
         print("Попал!")
     else:
         print("Мимо!")
 
+
 def move_x():
     """Сдвигает все шарики из списка по горизонтали """
-    
+
     for i in range(len(pool_x)):
-        vx = 5  #uniform(-1, 1) * 3   Будет каждый тик новую скорость каждому шару давать?
-        pool_x[i] = pool_x[i] + vx   
-    
+        if (pool_x[i] - pool_r[i] <= 0) or (pool_x[i] + pool_r[i] >= 800):
+            pool_vx[i] = -pool_vx[i]
+        vx = pool_vx[i]
+        pool_x[i] = pool_x[i] + vx
+
 
 def move_y():
     """Сдвигает все шарики из списка по вертикали """
-    
+
     for i in range(len(pool_y)):
-        vy = uniform(-1, 1) * 10   #Будет каждый тик новую скорость каждому шару давать?
+        if (pool_y[i] - pool_r[i] <= 0) or (pool_y[i] + pool_r[i] >= 600):
+            pool_vy[i] = -pool_vy[i]
+        vy = pool_vy[i]
         pool_y[i] = pool_y[i] + vy
 
+
 def draw():
-    "Рисует шарики из списка"
+    """Рисует шарики из списка"""
     for i in range(len(pool_x)):
         circle(screen, pool_color[i], (pool_x[i], pool_y[i]), pool_r[i])
 
@@ -76,25 +90,27 @@ finished = False
 
 while not finished:
     clock.tick(FPS)
+
+    new_ball()
+    c += 1
+    if c < 5:
+        pool_x.append(x)  # список х-координат центров шариков
+        pool_y.append(y)
+        pool_r.append(r)
+        pool_color.append(color)
+        pool_vx.append(uniform(-1, 1) * 50)
+        pool_vy.append(uniform(-1, 1) * 50)
+
+    move_x()
+    move_y()
+    draw()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("Кол-во попаданий в случайно возникающие шарики: ", k)
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             check()
-        
-    new_ball()
-    c+=1
-    if c < 5:
-        pool_x.append(x) #список х-координат центров шариков
-        pool_y.append(y) 
-        pool_r.append(r) 
-        pool_color.append(color)
-        
-    move_x()
-    move_y()
-    draw()
-    
     pygame.display.update()
     screen.fill(BLACK)
 
